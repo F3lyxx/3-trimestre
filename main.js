03cbb2febcb9fed47ac8f71448359b2c6f6a88db
F3lyxx <111387894+F3lyxx@users.noreply.github.com>
2023-11-22 12:42:51 -0300

    aa

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("Bem vindo a esta página, eu sou um alert, e você pode me excluir se desejar!")
+const aplausos = document.querySelector("#som_tecla_aplausos")
+function tocaSom(som) {
+    alert(som);
+}
+tocaSom("clap, clap, clap");
+let controle = "tocar";
+if (controle == "tocar") {
+    alert("Aúdio pronto para tocar");
+} else{
+    alert("Aúdio não esta pronto");
+}
+const buttons = document.querySelectorAll("button")
+let contator = 0;
+while (contador < buttons.length){
+    
+}
